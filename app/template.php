d63e7f672ea72df8da83c08892ed70acd22c827f
ukiras123 <ukiras@gmail.com>
2018-02-20 08:26:20 -0800

    hader file

--- app/template.php
+++ app/template.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
     <meta content="Make It Happen" property="og:title" />
     <meta content="https://s3-us-west-1.amazonaws.com/software-download-personal/picture/logo.png" property="og:image" />
