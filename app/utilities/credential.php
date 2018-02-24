16effd6a9f24b7c87925a0cd2f158f754b195512
ukiras123 <ukiras@gmail.com>
2018-02-23 23:05:01 -0800

    fixing credential

--- app/utilities/credential.php
+++ app/utilities/credential.php
@@ -3,7 +3,7 @@
 $DB_SERVER = "ec2-13-57-248-248.us-west-1.compute.amazonaws.com";
 //$DB_SERVER = "localhost";
 $DB_USERNAME = "root";
-$DB_PASSWORD = "rootKiran";
+$DB_PASSWORD = "rootkiran";
 $DB_NAME = "app";
 
 ?>
